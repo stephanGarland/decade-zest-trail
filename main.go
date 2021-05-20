1e8f3ab5749d4fdfc28680b6140c7075ee91daba
Stephan Garland <stephan.garland@logicmonitor.com>
2021-05-20 16:16:41 -0500

    shuffling

--- main.go
+++ main.go
@@ -3,13 +3,36 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-func main() {
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+func shuffle(deck []interface{}) []interface{} {
+	rand.Shuffle(len(deck), func(i, j int) {
+		deck[i], deck[j] = deck[j], deck[i]
+	})
+
+	return deck
+}
+
+func createDecks() ([]byte, []byte) {
 	merchData := merchantDeckCreator()
 	pointData := PointDeckCreator()
-	merchJson, _ := json.MarshalIndent(merchData, "", "    ")
-	pointJson, _ := json.MarshalIndent(pointData, "", "    ")
+	shuffledMerch := shuffle(merchData)
+	shuffledPoint := shuffle(pointData)
+	merchJson, _ := json.MarshalIndent(shuffledMerch, "", "    ")
+	pointJson, _ := json.MarshalIndent(shuffledPoint, "", "    ")
+
+	return merchJson, pointJson
+}
+
+func main() {
+	var merchJson, pointJson = createDecks()
 	fmt.Print(string(merchJson))
 	fmt.Print(string(pointJson))
+
 }
